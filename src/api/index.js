9c9c455a6d5990f678f662d62b726ab4dda5eeb5
Khemchand992 <km23mac2r10@student.nitw.ac.in>
2024-08-24 17:02:53 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+const baseURL =
+  "https://api.weatherapi.com/v1/current.json?key=e312b08cbcb442bdafa41250242408";
+
+export const getWeatherDataForCity = async (city) => {
+  const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
+  return await response.json();
+};
+
+export const getWeatherDataForLocation = async (lat, lon) => {
+  const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
+  return await response.json();
+};
